e5523eb25d30a6ea4429abe08ac6b201ff487003
Filipe Dias <filipediascontato@gmail.com>
2025-06-18 13:53:43 -0300

    feat: start to implement the chapter 2 with actor definition

---
+++ Chapter02/Actor.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <vector>
+
+class Actor
+{
+public:
+	enum State {
+		EActive,
+		EPaused,
+		EDead
+	};
+
+	Actor(class Game* game);
+	virtual ~Actor();
+
+	void Update(float deltaTime);
+	void UpdateComponent(float deltaTime);
+	virtual void UpdateActor(float deltaTime);
+
+	void AddComponent(class Component* component);
+	void RemoveComponent(class Component* component);
+
+private:
+	State mState;
+	Vector2 mPosition;
+	float mScale;
+	float mRotation;
+	std::vector<class Component*> mComponents;
+	class Game* mGame;
+};
